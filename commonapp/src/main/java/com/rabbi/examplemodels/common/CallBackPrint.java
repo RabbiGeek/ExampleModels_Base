e27df147aff7ace966eb839be899b1d5f3f6e19c
J.Z <zhaoji87@gmail.com>
2017-05-28 11:18:09 +0800

    update ignore

--- commonapp/src/main/java/com/rabbi/examplemodels/common/CallBackPrint.java
+++
@@ -1,8 +0,0 @@
-package com.rabbi.examplemodels.common;
-
-/**
- * Created by  on 11/30/15.
- * 回调打印接口
- */
-public interface CallBackPrint {
-}
--- commonapp/src/main/java/com/rabbi/examplemodels/common/config/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.rabbi.examplemodels.common.config;
-
-/**
- * Created by  on 11/27/15.
- * 存储应用常量信息
- */
-public class Constants {
-}
